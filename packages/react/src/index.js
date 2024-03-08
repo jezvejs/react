4769431378b5e5614a15879ec36fcf3114953a5d
Ilya Makarov <feesler@gmail.com>
2024-03-08 23:25:14 +0300

    + Added initial implementation of Collapsible component

--- packages/react/src/index.js
+++ packages/react/src/index.js
@@ -1,5 +1,6 @@
 export { Button } from './Components/Button/Button.jsx';
 export { Checkbox } from './Components/Checkbox/Checkbox.jsx';
+export { Collapsible } from './Components/Collapsible/Collapsible.jsx';
 export { Header } from './Components/Header/Header.jsx';
 export { Input } from './Components/Input/Input.jsx';
 export { Menu } from './Components/Menu/Menu.jsx';
---
+++ stories/Collapsible/Collapsible.stories.jsx
@@ -0,0 +1,91 @@
+// eslint-disable-next-line import/no-unresolved
+import '@jezvejs/react/style';
+import { Button, Collapsible } from '@jezvejs/react';
+
+import EditIcon from '../assets/icons/edit.svg';
+import DeleteIcon from '../assets/icons/del.svg';
+
+import './Collapsible.stories.scss';
+
+export default {
+    title: 'Components/Collapsible',
+    component: Collapsible,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+};
+
+const createContent = (content) => (
+    <div
+        className='collapsible-content-container'
+    >
+        {content ?? ''}
+    </div>
+);
+
+const createListContent = ({ itemsCount = 5, prefix = '', ...props } = {}) => (
+    <ul {...props}>
+        {Array(itemsCount).fill(0).map((_, index) => (
+            <li key={`${prefix}${index}`}>Item {index}</li>
+        ))}
+    </ul>
+);
+
+export const Default = {
+    args: {
+        className: 'simple',
+        header: 'Show',
+        children: (
+            <>
+                Content
+            </>
+        ),
+    },
+};
+
+export const Styled = {
+    args: {
+        className: 'styled',
+        header: 'Show',
+        children: createContent(createListContent({ prefix: 'styled' })),
+    },
+};
+
+export const Animated = {
+    args: {
+        className: 'styled',
+        header: 'Show',
+        animated: true,
+        children: createContent(createListContent({ prefix: 'animated' })),
+    },
+};
+
+const EditIconCustom = (props) => (
+    <EditIcon {...props} className='custom-header-icon' />
+);
+
+const DeleteIconCustom = (props) => (
+    <DeleteIcon {...props} className='custom-header-icon' />
+);
+
+const CustomHeaderButton = (props) => (
+    <Button
+        {...props}
+        className='custom-header-btn'
+        onClick={(e) => e.stopPropagation()}
+    />
+);
+
+export const CustomHeader = {
+    args: {
+        className: 'custom',
+        header: <>
+            <div className='custom-title'>Hover/focus to see controls</div>
+            <CustomHeaderButton icon={EditIconCustom} />
+            <CustomHeaderButton icon={DeleteIconCustom} />
+        </>,
+        animated: true,
+        children: createContent(createListContent({ prefix: 'animated' })),
+    },
+};
