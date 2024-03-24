dbfe054f63fa5f501009cc42d21434e710fa2afd
Ilya Makarov <feesler@gmail.com>
2024-03-24 23:30:16 +0300

    * [ControlledInput] Added support of 'handleValueProperty' option

--- packages/react/src/Components/ControlledInput/ControlledInput.jsx
+++ packages/react/src/Components/ControlledInput/ControlledInput.jsx
@@ -1,4 +1,5 @@
 import { isFunction } from '@jezvejs/types';
+import { useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import { Input } from '../Input/Input.jsx';
@@ -23,11 +24,52 @@ export const ControlledInput = (props) => {
         }
     };
 
+    /**
+     * Verifies update of input value and returns valid value
+     *
+     * @param {string} value
+     * @returns {string}
+     */
+    const handleValue = (value, prev) => {
+        const isValid = isValidValue(value);
+        return (isValid) ? value : prev;
+    };
+
+    /** Define setter for 'value' property of input to prevent invalid values */
+    const observeInputValue = (input) => {
+        const elementPrototype = Object.getPrototypeOf(input);
+        const descriptor = Object.getOwnPropertyDescriptor(elementPrototype, 'value');
+
+        Object.defineProperty(input, 'value', {
+            get() {
+                return descriptor.get.call(this);
+            },
+            set(value) {
+                if (value === this.value) {
+                    return;
+                }
+
+                descriptor.set.call(this, handleValue(value, this.value));
+            },
+        });
+    };
+
+    const contentRef = useCallback((node) => {
+        if (!node) {
+            return;
+        }
+
+        if (props.handleValueProperty) {
+            observeInputValue(node);
+        }
+    }, [props.handleValueProperty]);
+
     const inputProps = {
         ...props,
         onBeforeInputCapture: validateInput,
         onPasteCapture: validateInput,
         onKeyPressCapture: validateInput,
+        ref: contentRef,
     };
 
     return (
